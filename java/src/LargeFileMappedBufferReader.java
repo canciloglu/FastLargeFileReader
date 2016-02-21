858828d9caf5701657174acb955dfb0b687341ea
Can Çiloğlu <canciloglu@yahoo.com>
2016-02-21 23:06:43 +0200

    Update LargeFileMappedBufferReader.java

--- java/src/LargeFileMappedBufferReader.java
+++ java/src/LargeFileMappedBufferReader.java
@@ -1,3 +1,4 @@
+
 /**
  *  Created on: Feb 21, 2016
  *      Author: Can Ciloglu 
@@ -12,32 +13,31 @@ public class LargeFileMappedBufferReader {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
 		long lineCount = 0;
-		
-	    FileChannel fc = null;
-	    try {
-	        fc = new RandomAccessFile("136mb.txt", "r").getChannel();
-	        MappedByteBuffer mb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
-	        while (mb.hasRemaining()) {
-	            byte byteRead = mb.get();
-	            if(byteRead == '\n') {
-	            	lineCount++;
-	            }
-	        }
-	    } catch (IOException e) {
-	    	e.printStackTrace();
-	    } finally {
-	    	if(fc != null){
+
+		FileChannel fc = null;
+		try {
+			fc = new RandomAccessFile("136mb.txt", "r").getChannel();
+			MappedByteBuffer mb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+			while (mb.hasRemaining()) {
+				byte byteRead = mb.get();
+				if (byteRead == '\n') {
+					lineCount++;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (fc != null) {
 				try {
 					fc.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-	    	}
+			}
 		}
-	    
-	    long end = System.currentTimeMillis();
-	    
-	    System.out.println("Number of lines = " + lineCount + " processed in " + (end - start) + " ms");
-	}
 
+		long end = System.currentTimeMillis();
+
+		System.out.println("Number of lines = " + lineCount + " processed in " + (end - start) + " ms");
+	}
 }
